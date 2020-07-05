f306c479ef3e31047a41f1e741484bf65fad6560
Made Satria Wibawa <mdsatria@gmail.com>
2020-07-05 08:54:15 +0800

    fix

--- cpp/6_binary_search.cpp
+++ cpp/6_binary_search.cpp
@@ -12,20 +12,21 @@ int binarySearch(int arr[], int size, int num);
 int main()
 {
     // data array yang digunakan untuk tempat mencari nilai
-    int A[5] = {1,2,3,4,5};
+    int A[] = {1,1,2,3,4,5};
+    int s = sizeof(A) / sizeof(A[0]);
     // dua nilai yang akan dicari, yaitu num_1 = 2 dan num_2 = 10
-    int num_1 = 2;
+    int num_1 = 1;
     int num_2 = 10;
      // variabel menampung indeks hasil pencarian
     int hasil;
 
     // gunakan fungsi linearSeach. tempat mencari di array A dengan elemen sebanyak 5, yang dicari adalah num_1 (2)
-    hasil = binarySearch(A, 5, num_1);
+    hasil = binarySearch(A, s, num_1);
     // kondisi jika data yang dicari ada di array A
     if (hasil!=-1)
     {
         // tampilkan pesan berhasil ditemukan
-        cout << "Bilangan " << num_1 << " ditemukan pada array A!" << endl;
+        cout << "Bilangan " << num_1 << " ditemukan pada array A di index ke-" << hasil << endl;
     }
     // jika data tidak ditemukan pada array A 
     else
@@ -35,12 +36,12 @@ int main()
     }
 
     // gunakan fungsi binarySeach. tempat mencari di array A dengan elemen sebanyak 5, yang dicari adalah num_2 (10)
-    hasil = binarySearch(A, 5, num_2);
+    hasil = binarySearch(A, s, num_2);
     // kondisi jika data yang dicari ada di array A
     if (hasil!=-1)
     {
          // tampilkan pesan berhasil ditemukan
-        cout << "Bilangan " << num_2 << " ditemukan pada array A!" << endl;
+        cout << "Bilangan " << num_2 << " ditemukan pada array A di index ke-" << hasil << endl;
     }
      // jika data tidak ditemukan pada array A
     else
@@ -62,45 +63,43 @@ int main()
 //          num : nilai yang akan dicari
 int binarySearch(int arr[], int size, int num)
 {
-    // deklarasi variabel hasil untuk menampung hasil pencarian. 
+    // deklarasi variabel result untuk menampung hasil pencarian. 
     // nilai default adalah -1
-    int hasil = -1;
-    // variabel i untuk indeks pertama pada sub-array
-    int i = 0;
-    // variabel j untuk indeks terakhir pada sub-array
-    int j = size-1;
-    // variabel m untuk indeks tengah pada sub-array
-    int m;
+    int result = -1;
+    // variabel left untuk indeks pertama pada sub-array
+    int left = 0;
+    // variabel right untuk indeks terakhir pada sub-array
+    int right = size-1;
+    // variabel middle untuk indeks tengah pada sub-array
+    int middle;
 
     // perulangan hingga ukuran sub-array menjadi satu elemen
-    while (i<j)
+    while (left<=right)
     {
         // cari indeks tengah pada sub-array
-        m = (i+j)/2;
-        // bandingkan nilai 'num' dengan larik 'arr' pada indeks ke-m. TRUE jika sama
-        if (num==arr[m])
+        middle = (left + right) / 2;
+        // bandingkan nilai 'num' dengan larik 'arr' pada indeks ke-middle. TRUE jika sama
+        if (num==arr[middle])
         {
-            // jika sama maka hasil diubah nilainya dengan m
-            hasil = m;
-            // stop perulangan, kembalikan nilai hasil
-            return hasil;
+            // jika sama maka hasil diubah nilainya dengan middle
+            result = middle;
         }
 
-        // cek apakah nilai 'num' lebih besar daripada nilai 'arr' pada indeks ke-m. TURE jika lebih besar
-        if (num>arr[m])
+        // cek apakah nilai 'num' lebih besar daripada nilai 'arr' pada indeks ke-middle. TURE jika lebih besar
+        if (num>arr[middle])
         {
-            // jika kondisi benar maka indeks pertama sub-array menjadi m+1
-            i = m + 1;
+            // jika kondisi benar maka indeks pertama sub-array menjadi middle+1
+            left = middle + 1;
         }
         // jika kondisi salah
         else
         {
-            // indeks terakhir pada sub-array menjadi m
-            j = m;
+            // indeks terakhir pada sub-array menjadi middle
+            right = middle - 1;
         }
     }
     // kembalikan nilai default hasil (-1).
     // artinya perulangan telah selesai sampai elemen akhir pada 'arr', data tidak ditemukan
-    return hasil;
+    return result;
 }
 
