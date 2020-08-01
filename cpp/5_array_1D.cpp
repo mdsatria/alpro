6206cb4d44288b483a8549afb55691f94473b612
Made Satria Wibawa <mdsatria@gmail.com>
2020-08-01 14:06:07 +0800

    array

--- cpp/5_array_1D.cpp
+++ cpp/5_array_1D.cpp
@@ -1,5 +1,6 @@
 // import library untuk fungsi input output
 #include <iostream>
+#include "5_funcArray.cpp"
 // gunakan namespace pada iostream
 using namespace std;
 
@@ -7,10 +8,20 @@ using namespace std;
 int main()
 {
     
-    int A[5] = {3, 4, 5, 6, 2};
-    
-    for (int i=0; i<5; i++)
-    {
-        cout << A[i] << " " ;
-    }
+    int A[5];
+    int n = sizeof(A) / sizeof(A[0]);
+
+    // panggil procedure readArray() untuk memasukkan input dari user ke array A
+    readArray(A, n);
+    // panggil procedure printArray() untuk mencetak semua nilai dari array A
+	printArray(A, n);
+    // panggil function findMax() untuk mencari kemudian tampilkan nilai terbesar di array A
+	cout << "Bilangan terbesar adalah : " << findMax(A, n) << endl;
+    // panggil function findMin() untuk mencari kemudian tampilkan nilai terkecil di array A
+    cout << "Bilangan terkecil adalah : " << findMin(A, n) << endl;
+    // panggil procedure reverseArray() untuk membalikkan posisi elemen pada array A
+    reverseArray(A, n);
+    // tampilkan hasil reverseArray()
+    printArray(A, n);
+
 }
